26c5c0f3c4d94bed65181af4cf26e7c0f0a853f0
oldcapone <ivanov.planeta@gmail.com>
2022-10-07 17:31:08 +0300

    ADD menu

---
+++ modules.py
@@ -0,0 +1,12 @@
+# 1. встроенные модули
+import random
+import os
+import sys
+import shutil
+
+print(os.getcwd())
+#Список файлов и папок
+print(os.listdir())
+
+#Создание папки
+os.mkdir()
---
+++ my_chet.py
@@ -0,0 +1,30 @@
+def start():
+    chet = 0
+    name = "НЕТ"
+    histoty = []
+    while True:
+        print('1. пополнение счета')
+        print('2. покупка')
+        print('3. история покупок')
+        print('4. выход')
+        print(f'на счету {chet}')
+
+        choice = input('Выберите пункт меню: ')
+        if choice == '1':
+            plus = float(input("Введите сумму на сколько пополнить счет: "))
+            chet +=plus
+            print(chet)
+        elif choice == '2':
+            buy = float(input("Введите сумму покупки: "))
+            if buy>chet:
+                print("Денег не хватает")
+            else:
+                chet-=buy
+                name = (input("Введите название покупки: "))
+                histoty.append((name , buy))
+        elif choice == '3':
+            print(histoty)
+        elif choice == '4':
+            break
+        else:
+            print('Неверный пункт меню')
